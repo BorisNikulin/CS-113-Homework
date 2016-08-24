8dc78ebe605d2344e7f8bf628e34a2b4f3eb98a6
Boris Nikulin <NikulinBE@gmail.com>
2016-08-24 12:47:10 -0700

    FizzBuzz code
    Full FizzBuzz code in main method

--- src/main.java
+++ src/main.java
@@ -4,8 +4,21 @@ public class main
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, World!");
-		System.out.println("Let's see how a change affects git.");
+		//FizzBuzz
+		for(int i = 1; i <= 100; ++i)
+		{
+			if(i % 3 == 0 || i % 5 == 0)
+			{
+				String output = "";
+				output += (i % 3 == 0) ? "Fizz" : "";
+				output += (i % 5 == 0) ? "Buzz" : "";
+				System.out.println(output);
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
 	}
 
 }
