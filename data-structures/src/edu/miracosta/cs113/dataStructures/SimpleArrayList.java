3f9e330c7bb5a5b42fb6101c3ea7d8691d42c28f
Boris Nikulin <NikulinBE@gmail.com>
2016-09-18 00:56:50 -0700

    Data Structures SimpleArayList remove and set + formatting + doc changes
    remove(int) and set + tests added.
    copy has changed to copyLeft and copyRight has been created.
    assert checks on copy methods and on GROWTH_MULTIPLIER.

--- data-structures/src/edu/miracosta/cs113/dataStructures/SimpleArrayList.java
+++ data-structures/src/edu/miracosta/cs113/dataStructures/SimpleArrayList.java
@@ -7,15 +7,25 @@ import java.util.List;
 
 public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>, List<E>, Iterable<E>
 {
-	private static double	GROWTH_MULTIPLIER	= 2;
+	private static double	GROWTH_MULTIPLIER	= 3.0 / 2.0;
 
 	private E[]				data;
 	private int				size;
 
-	@SuppressWarnings("unchecked")
+	static
+	{
+		assert GROWTH_MULTIPLIER > 1 : "GROWTH_MULTIPLIER (" + GROWTH_MULTIPLIER + ") need to be greater than 1";
+	}
+
 	public SimpleArrayList ()
 	{
-		data = (E[]) new Object[10];
+		this (10);
+	}
+
+	@SuppressWarnings("unchecked")
+	public SimpleArrayList (int initialCapacity)
+	{
+		data = (E[]) new Object[initialCapacity];
 		size = 0;
 	}
 
@@ -38,7 +48,7 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 	/**
 	 * Grows the data array by {@link #GROWTH_MULTIPLIER} and copies the data
 	 * over to the new array using
-	 * {@link #copy(Object[], int, Object[], int, int)}
+	 * {@link #copyLeft(Object[], int, Object[], int, int)}
 	 */
 	@SuppressWarnings("unchecked")
 	private void reallocate ()
@@ -46,20 +56,27 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 		E[] temp = data;
 		data = (E[]) new Object[(int) (size * GROWTH_MULTIPLIER)];
 
-		copy (data, 0, temp, 0, size);
+		copyLeft (data, 0, temp, 0, size);
 		// TODO note optimizations like leaving a gap for space to add and thus
 		// not re copying that stuff again
 		// maybe for later
 	}
 
 	/**
-	 * Transfers from start to end of one array to another like memncpy in c.
+	 * Transfers from start to end of one array to another like memncpy in c
+	 * from the left ie. from the end to the start.
 	 * 
 	 * <p>
-	 * Requires that the destination have space like memncpy.
+	 * Requires that the destination and source have the space to take and set
+	 * to.
 	 * </p>
 	 * <p>
-	 * Can copy to and from the same array.
+	 * Can copy to and from the same array if start1 >= start2
+	 * </p>
+	 * <p>
+	 * The pre-reqs formally:
+	 * {@code assert dest.length >= numElements - start1 && source.length >= numElements - start2;}
+	 * and {@code if (dest == source) assert start1 >= start2;}
 	 * </p>
 	 * 
 	 * @param dest
@@ -74,10 +91,53 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 	 *            the number of elements to copy
 	 * 
 	 * @return the destination array
+	 * @see #copyRight(Object[], int, Object[], int, int)
 	 */
-	private <U> U[] copy (U[] dest, int start1, U[] source, int start2, int numElements)
+	private <T> T[] copyLeft (T[] dest, int start1, T[] source, int start2, int numElements)
 	{
+		assert (dest.length >= numElements - start1
+				&& source.length >= numElements - start2) : "exceeded start and or dest array";
+		if (dest == source) assert start1 >= start2 : "use copyRight version";
+
 		for (int i = numElements - 1; i >= 0; --i)
+		{
+			dest[start1 + i] = source[start2 + i];
+		}
+		return dest;
+	}
+
+	/**
+	 * Transfers from start to end of one array to another like memncpy in c
+	 * from the right ie. from the start to the end.
+	 * 
+	 * <p>
+	 * Requires that the destination has enough space like memncpy.
+	 * </p>
+	 * <p>
+	 * Can copy to and from the same array if start1 <= start2
+	 * </p>
+	 * 
+	 * @param dest
+	 *            destination array (must have sufficient space)
+	 * @param start1
+	 *            where to start copying to
+	 * @param source
+	 *            source array
+	 * @param start2
+	 *            where to start copying from
+	 * @param numElements
+	 *            the number of elements to copy
+	 * 
+	 * @return the destination array
+	 * @see #copyLeft(Object[], int, Object[], int, int)
+	 */
+	private <T> T[] copyRight (T[] dest, int start1, T[] source, int start2, int numElements)
+	{
+		assert (dest.length >= numElements - start1
+				&& source.length >= numElements - start2) : "exceeded start and or dest array";
+		if (dest == source) assert start1 <= start2 : "use copyLeft version";
+
+		for (int i = 0; i < numElements; ++i)
 		{
 			dest[start1 + i] = source[start2 + i];
 		}
@@ -109,14 +169,14 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 
 		checkReallocation ();
 
-		if(index != size)
+		if (index != size)
 		{
-			copy(data, index + 1, data, index, size - index);
+			copyLeft (data, index + 1, data, index, size - index);
 		}
 		data[index] = e;
 
-		size++;
-		modCount++;
+		++size;
+		++modCount;
 	}
 
 	@Override
@@ -153,15 +213,20 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 	@Override
 	public E remove (int index)
 	{
-
-		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
+		E temp = data[index];
+		copyRight (data, index, data, index + 1, size - index - 1);
+		--size;
+		++modCount;
+		return temp;
 	}
 
 	@Override
 	public E set (int index, E e)
 	{
-
-		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
+		boundsCheck (index);
+		E temp = data[index];
+		data[index] = e;
+		return temp;
 	}
 
 	@Override
@@ -174,21 +239,18 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 	@Override
 	public boolean addAll (Collection<? extends E> c)
 	{
-
 		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
 	}
 
 	@Override
 	public boolean contains (Object o)
 	{
-
 		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
 	}
 
 	@Override
 	public boolean containsAll (Collection<?> c)
 	{
-
 		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
 	}
 
@@ -201,21 +263,18 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 	@Override
 	public boolean remove (Object o)
 	{
-
 		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
 	}
 
 	@Override
 	public boolean removeAll (Collection<?> c)
 	{
-
 		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
 	}
 
 	@Override
 	public boolean retainAll (Collection<?> c)
 	{
-
 		throw new UnsupportedOperationException ("I'll get around to this at some point if I need it. :D");
 	}
 
@@ -237,14 +296,14 @@ public class SimpleArrayList<E> extends AbstractList<E> implements Collection<E>
 	{
 		if (a.length >= size)
 		{
-			copy (a, 0, (T[]) data, 0, size);
+			copyLeft (a, 0, (T[]) data, 0, size);
 			if (a.length > size)
 			{
 				a[a.length - 1] = null;
 			}
 			return a;
 		}
-		return (T[]) copy (new Object[size], 0, data, 0, size);
+		return (T[]) copyLeft (new Object[size], 0, data, 0, size);
 	}
 
 }
--- data-structures/src/edu/miracosta/cs113/test/SimpleArrayListTest.java
+++ data-structures/src/edu/miracosta/cs113/test/SimpleArrayListTest.java
@@ -44,9 +44,9 @@ public class SimpleArrayListTest
 	@Test
 	public void testIsEmpty ()
 	{
-		assertThat (emptyList.isEmpty (), is (true));
 		assertThat (intList.isEmpty (), is (false));
 		assertThat (stringList.isEmpty (), is (false));
+		assertThat (emptyList.isEmpty (), is (true));
 	}
 
 	@Test
@@ -54,24 +54,31 @@ public class SimpleArrayListTest
 	{
 		intList.add (0, 10);
 		stringList.add (0, "f");
+		assertThat (intList.size (), is (11));
+		assertThat (stringList.size (), is (6));
 		assertThat (intList.get (0), is (10));
 		assertThat (stringList.get (0), is ("f"));
 		assertThat (intList.get (1), is (0));
 		assertThat (stringList.get (1), is ("a"));
-		
+
 		intList.add (4, 11); // [10, 0, 1, 2, 3, 4...
 		stringList.add (4, "g"); // [f, a ,b, c, d, e...
+		assertThat (intList.size (), is (12));
+		assertThat (stringList.size (), is (7));
 		assertThat (intList.get (4), is (11));
 		assertThat (stringList.get (4), is ("g"));
 		assertThat (intList.get (5), is (3));
 		assertThat (stringList.get (5), is ("d"));
-		
+
 		emptyList.add (0, 1.0);
+		assertThat (emptyList.size (), is (1));
 		assertThat (emptyList.get (0), is (1.0));
 		emptyList.add (0, 2.0);
+		assertThat (emptyList.size (), is (2));
 		assertThat (emptyList.get (0), is (2.0));
 		assertThat (emptyList.get (1), is (1.0));
 		emptyList.add (1, 3.0); // [2 , 1] -> [2, 3, 1]
+		assertThat (emptyList.size (), is (3));
 		assertThat (emptyList.get (1), is (3.0));
 		assertThat (emptyList.get (2), is (1.0));
 	}
@@ -87,4 +94,29 @@ public class SimpleArrayListTest
 		assertThat (stringList.get (5), is ("f"));
 	}
 
+	@Test
+	public void testSet ()
+	{
+		intList.set (0, 100);
+		stringList.set (0, "z");
+		assertThat (intList.size (), is (10));
+		assertThat (stringList.size (), is (5));
+		assertThat (intList.get (0), is (100));
+		assertThat (stringList.get (0), is ("z"));
+		assertThat (intList.get (1), is (1));
+	}
+
+	@Test
+	public void testRemoveIndex ()
+	{
+		intList.remove (0);
+		stringList.remove (2);
+		assertThat (intList.size (), is (9));
+		assertThat (stringList.size (), is (4));
+		assertThat (intList.get (0), is (1));
+		assertThat (stringList.get (2), is ("d"));
+		assertThat (intList.get (1), is (2));
+		assertThat (stringList.get (3), is ("e"));
+
+	}
 }
