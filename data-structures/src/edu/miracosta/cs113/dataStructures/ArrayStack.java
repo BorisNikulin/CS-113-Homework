98d77f49ad49aebd7e41985f409a4961221785d8
Boris Nikulin <NikulinBE@gmail.com>
2016-10-07 19:08:43 -0700

    Data Structures ArrayStack/ListStack added equals, hashCode, toString, and iterator

--- data-structures/src/edu/miracosta/cs113/dataStructures/ArrayStack.java
+++ data-structures/src/edu/miracosta/cs113/dataStructures/ArrayStack.java
@@ -1,7 +1,11 @@
 
 package edu.miracosta.cs113.dataStructures;
 
-public class ArrayStack<E>
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
+public class ArrayStack<E> implements Iterable<E>
 {
 	public SimpleArrayList<E> data;
 
@@ -42,4 +46,89 @@ public class ArrayStack<E>
 		int index = data.indexOf (o);
 		return (index != -1) ? index + 1 : index;
 	}
+	
+	public String toString()
+	{
+		return data.stream ()
+				.map (Object::toString)
+				.collect (Collectors.joining (",", "[", "]"));
+	}
+
+	@Override
+	public Iterator<E> iterator ()
+	{
+		return new StackItr();
+	}
+	
+	@Override
+	public int hashCode ()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode ());
+		return result;
+	}
+
+	@Override
+	public boolean equals (Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if(obj instanceof Collection)
+		{
+			@SuppressWarnings("rawtypes") Collection other = (Collection) obj;
+			if(data.size () != other.size ())
+			{
+				return false;
+			}
+			@SuppressWarnings("rawtypes") Iterator cItr = other.iterator();
+			for(int i = data.size() - 1; i >= 0; --i)
+			{
+				if(!data.get (i).equals (cItr.next()))
+				{
+					return false;
+				}
+			}
+		}
+		if (getClass () != obj.getClass ())
+		{
+			return false;
+		}
+		@SuppressWarnings("rawtypes") ArrayStack other = (ArrayStack) obj;
+		if (data == null)
+		{
+			if (other.data != null)
+			{
+				return false;
+			}
+		}
+		else if (!data.equals (other.data))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	private class StackItr implements Iterator<E>
+	{
+		Iterator<E> dataItr = data.iterator ();
+		@Override
+		public boolean hasNext ()
+		{
+			return dataItr.hasNext ();
+		}
+
+		@Override
+		public E next ()
+		{
+			return dataItr.next ();
+		}
+	}
+	
 }
--- data-structures/src/edu/miracosta/cs113/dataStructures/ListStack.java
+++ data-structures/src/edu/miracosta/cs113/dataStructures/ListStack.java
@@ -1,7 +1,11 @@
 
 package edu.miracosta.cs113.dataStructures;
 
-public class ListStack<E>
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
+public class ListStack<E> implements Iterable<E>
 {
 	// TODO make a double ended singly linked list and use it
 	public SimpleLinkedList<E> data;
@@ -37,4 +41,80 @@ public class ListStack<E>
 		int index = data.indexOf (o);
 		return (index != -1) ? index + 1 : index;
 	}
+	
+	@Override
+	public boolean equals (Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if(obj instanceof Collection)
+		{
+			@SuppressWarnings("rawtypes") Collection other = (Collection) obj;
+			if(data.size () != other.size ())
+			{
+				return false;
+			}
+			@SuppressWarnings("rawtypes") Iterator cItr = other.iterator();
+			for(int i = data.size() - 1; i >= 0; --i)
+			{
+				if(!data.get (i).equals (cItr.next()))
+				{
+					return false;
+				}
+			}
+		}
+		if (getClass () != obj.getClass ())
+		{
+			return false;
+		}
+		@SuppressWarnings("rawtypes") ArrayStack other = (ArrayStack) obj;
+		if (data == null)
+		{
+			if (other.data != null)
+			{
+				return false;
+			}
+		}
+		else if (!data.equals (other.data))
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public String toString()
+	{
+		return data.stream ()
+				.map (Object::toString)
+				.collect (Collectors.joining (",", "[", "]"));
+	}
+	
+	@Override
+	public Iterator<E> iterator ()
+	{
+		return new StackItr();
+	}
+	
+	private class StackItr implements Iterator<E>
+	{
+		Iterator<E> dataItr = data.iterator ();
+		@Override
+		public boolean hasNext ()
+		{
+			return dataItr.hasNext ();
+		}
+
+		@Override
+		public E next ()
+		{
+			return dataItr.next ();
+		}
+	}
+	
 }
