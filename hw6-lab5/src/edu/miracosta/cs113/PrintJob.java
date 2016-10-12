2eb8f1acaf076e4f3060d5f86b70b4565d5f7c46
Boris Nikulin <NikulinBE@gmail.com>
2016-10-11 17:09:02 -0700

    HW6 Project5 PrintJob mock obj

---
+++ hw6-lab5/src/edu/miracosta/cs113/PrintJob.java
@@ -0,0 +1,38 @@
+
+package edu.miracosta.cs113;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 
+ * A mock object for a print job to implemented in the future (or never :D)
+ * 
+ * @author Boris
+ */
+public class PrintJob
+{
+	private long id;
+	private int pageCount;
+	
+	public PrintJob(int pageCount)
+	{
+		this (ThreadLocalRandom.current ().nextLong (), pageCount);
+	}
+	
+	public PrintJob(long id, int pageCount)
+	{
+		this.id = id;
+		this.pageCount = pageCount;
+	}
+
+	public long getId ()
+	{
+		return id;
+	}
+
+	public int getPageCount ()
+	{
+		return pageCount;
+	}
+
+}
