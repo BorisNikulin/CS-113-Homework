f2825e212b883e84bc8eb33ed5be748a2ea3d379
Boris Nikulin <NikulinBE@gmail.com>
2016-11-10 23:16:06 -0800

    HW6 Project5 PriorityPrintQueue Initial implementation

--- hw6-lab5/src/edu/miracosta/cs113/PriorityPrintQueue.java
+++ hw6-lab5/src/edu/miracosta/cs113/PriorityPrintQueue.java
@@ -2,13 +2,11 @@
 package edu.miracosta.cs113;
 
 import java.util.AbstractQueue;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Queue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Represents a thread safe priority queue for print jobs. Priority is
@@ -17,96 +15,133 @@ import java.util.concurrent.TimeUnit;
  * @author Boris
  * 
  */
-public class PriorityPrintQueue extends AbstractQueue<PrintJob> implements BlockingQueue<PrintJob>
+public class PriorityPrintQueue extends AbstractQueue<PrintJob>
 {
+	// TODO eventually finish making this class efficiently thread safe
+	// was left off at commit efc3e55a6fde6b
+
 	// TODO make my own queue (my own heap + priority queue eventually)
 	Queue<PrintJob>	lessThan10Pages	= new LinkedList<> ();
 	Queue<PrintJob>	lessThan20Pages	= new LinkedList<> ();
 	Queue<PrintJob>	moreThan20Pages	= new LinkedList<> ();
 
+	/**
+	 * Gets the queue from which the next element will be retrieved
+	 * 
+	 * @return the to retrieve next element from or null if empty
+	 */
+	private Queue<PrintJob> getHeadQueue ()
+	{
+		if (lessThan10Pages.size () > 0)
+		{
+			return lessThan10Pages;
+		}
+		else if (lessThan20Pages.size () > 0)
+		{
+			return lessThan20Pages;
+		}
+		else if (moreThan20Pages.size () > 0)
+		{
+			return moreThan20Pages;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Gets the queue for the a PrintJob to be inserted based on page count
+	 * 
+	 * @param pageCount
+	 *            - The page count on which queue to return
+	 * @return the queue in which a PrintJob with page count would go
+	 */
+	private Queue<PrintJob> getQueue (int pageCount)
+	{
+		if (pageCount < 10)
+		{
+			return lessThan10Pages;
+		}
+		else if (pageCount < 20)
+		{
+			return lessThan20Pages;
+		}
+		else // >= 20
+		{
+			return moreThan20Pages;
+		}
+	}
+
 	@Override
 	public boolean offer (PrintJob e)
 	{
 		Objects.requireNonNull (e, "Elements can not be null");
+		return getQueue (e.getPageCount ()).offer (e);
 
-		return true;
 	}
 
 	@Override
 	public PrintJob peek ()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Queue<PrintJob> headQueue = getHeadQueue ();
+		return headQueue != null ? headQueue.peek () : null;
 	}
 
 	@Override
 	public PrintJob poll ()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Queue<PrintJob> headQueue = getHeadQueue ();
+		return headQueue != null ? headQueue.poll () : null;
 	}
 
 	@Override
 	public Iterator<PrintJob> iterator ()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new PriorityPrintQueueItr ();
 	}
 
 	@Override
 	public int size ()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return lessThan10Pages.size () + lessThan20Pages.size () + moreThan20Pages.size ();
 	}
 
-	@Override
-	public int drainTo (Collection<? super PrintJob> c)
+	private class PriorityPrintQueueItr implements Iterator<PrintJob>
 	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	@Override
-	public int drainTo (Collection<? super PrintJob> c, int maxElements)
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		Iterator<PrintJob>	lessThan10Itr	= lessThan10Pages.iterator ();
+		Iterator<PrintJob>	lessThan20Itr	= lessThan20Pages.iterator ();
+		Iterator<PrintJob>	moreThan20Itr	= moreThan20Pages.iterator ();
 
-	@Override
-	public boolean offer (PrintJob e, long timeout, TimeUnit unit) throws InterruptedException
-	{
-		// TODO Auto-generated method stub
-		return false;
-	}
+		@Override
+		public boolean hasNext ()
+		{
+			return lessThan10Itr.hasNext () || lessThan20Itr.hasNext () || moreThan20Itr.hasNext ();
+		}
 
-	@Override
-	public PrintJob poll (long timeout, TimeUnit unit) throws InterruptedException
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+		@Override
+		public PrintJob next ()
+		{
+			if (!hasNext ())
+			{
+				throw new NoSuchElementException ();
+			}
 
-	@Override
-	public void put (PrintJob e) throws InterruptedException
-	{
-		// TODO Auto-generated method stub
+			if (lessThan10Itr.hasNext ())
+			{
+				return lessThan10Itr.next ();
+			}
+			else if (lessThan20Itr.hasNext ())
+			{
+				return lessThan20Itr.next ();
+			}
+			else // moreThan20Itr.hasNext()
+			{
+				return moreThan20Itr.next ();
+			}
+		}
 
-	}
-
-	@Override
-	public int remainingCapacity ()
-	{
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public PrintJob take () throws InterruptedException
-	{
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
