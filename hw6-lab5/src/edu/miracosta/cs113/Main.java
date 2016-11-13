e497041f5bb78bd85b09225cded2150e8cbbbf51
Boris Nikulin <NikulinBE@gmail.com>
2016-11-13 03:14:01 -0800

    HW6 Project5 Main printDuration function
    The function gives the duration needed to print the given print jobs using the given printers.

--- hw6-lab5/src/edu/miracosta/cs113/Main.java
+++ hw6-lab5/src/edu/miracosta/cs113/Main.java
@@ -1,20 +1,45 @@
 
 package edu.miracosta.cs113;
 
+import java.sql.Wrapper;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Random;
+import java.util.function.LongSupplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main
 {
 
-	public static final int SEED = 46525;
+	public static final int	SEED				= 46525;
+	public static final int	PRINT_JOB_LENGTH	= 100;
 
 	public static void main (String[] args)
 	{
-		PriorityPrintQueue ppq = generatePrintJobs (PriorityPrintQueue.collector (), SEED, 100);
-		List<PrintJob> printJobInsertionOrder = generatePrintJobs (Collectors.toList (), SEED, 100);
+		Duration printDuration = Duration.ZERO;
+
+		// synthetic way to receive print jobs
+		// to get insertion order a wrapper on the queue
+		// or an extra layer of code when a print job is received can be done
+		// id like to try using ReactFX or RxJava for that react paradigm
+		// stream/subscriber thing ...
+		// but ill just do generic uninspired code
+		PriorityPrintQueue ppq = generatePrintJobs (PriorityPrintQueue.collector (), SEED, PRINT_JOB_LENGTH);
+		List<PrintJob> printJobInsertionOrder = generatePrintJobs (Collectors.toList (), SEED, PRINT_JOB_LENGTH);
+		// 10 pages per minute
+		Printer printer = new Printer (Duration.ofMinutes (1).dividedBy (10));
+		List<Printer> printers = Arrays.asList (printer, printer, printer);
+
+		// synthetic add since print jobs were added synthetically
+		// 1 minute wait between each print job request
+		printDuration = printDuration.plusMinutes (PRINT_JOB_LENGTH);
 
 		System.out.println (ppq.toString ().replaceAll (",", ",\n"));
 		System.out.println ("\n----------------------\n");
@@ -40,6 +65,83 @@ public class Main
 		{
 			return collector.finisher ().apply (container);
 		}
+	}
+
+	/**
+	 * A fucntion that caluculates the duration of time to print a sequence of
+	 * {@link PrintJob} s using the per page print duration of the given
+	 * sequence of {@link Printer} s.
+	 * 
+	 * @param printJobs
+	 *            - the PrintJobs to fake print
+	 * @param printers
+	 *            - the printers doing the fake printing
+	 * @param numberOfPrinters
+	 *            - best guess at the provided number of printers
+	 * @return the duration of time to print the provided print jobs using the
+	 *         provided printers or null if there are no printers
+	 * 
+	 * @see #printDuration(Iterable, Collection)
+	 */
+	public static Duration printDuration (Iterable<PrintJob> printJobs, Iterable<Printer> printers,
+			int numberOfPrinters)
+	{
+		class PrinterAndPrintDurationWrapper
+		{
+			public Printer	printer;
+			public Duration	totalPrintDuration;
+
+			public PrinterAndPrintDurationWrapper (Printer printer, Duration totalPrintDuration)
+			{
+				this.printer = printer;
+				this.totalPrintDuration = totalPrintDuration;
+			}
+		}
+
+		if (!printers.iterator ().hasNext () || numberOfPrinters <= 0)
+		{
+			return null;
+		}
+
+		PriorityQueue<PrinterAndPrintDurationWrapper> printDurations = new PriorityQueue<> (
+				numberOfPrinters,
+				Comparator.comparing (wrapper -> wrapper.totalPrintDuration));
+
+		for (Printer printer : printers)
+		{
+			printDurations.offer (new PrinterAndPrintDurationWrapper (printer, Duration.ZERO));
+		}
+
+		for (PrintJob pj : printJobs)
+		{
+			PrinterAndPrintDurationWrapper firstAvailblePrinter = printDurations.poll ();
+			firstAvailblePrinter.totalPrintDuration = firstAvailblePrinter.totalPrintDuration.plus (
+					firstAvailblePrinter.printer.getPrintTimePerPage ().multipliedBy (pj.getPageCount ()));
+			printDurations.offer (firstAvailblePrinter);
+		}
+
+		return printDurations.stream ()
+				.map (wrapper -> wrapper.totalPrintDuration)
+				.reduce (Duration::plus)
+				.orElse (Duration.ZERO);
+	}
+
+	/**
+	 * A wrapper for {@link #printDuration(Iterable, Iterable, int)} where the
+	 * {@code numberOfPrinters} is the size of the printers collection.
+	 * 
+	 * @param printJobs
+	 *            - the PrintJobs to fake print
+	 * @param printers
+	 *            - the printers doing the fake printing
+	 * @return the duration of time to print the provided print jobs using the
+	 *         provided printers or null if there are no printers
+	 * 
+	 * @see #printDuration(Iterable, Iterable, int)
+	 */
+	public static Duration printDuration (Iterable<PrintJob> printJobs, Collection<Printer> printers)
+	{
+		return printDuration (printJobs, printers, printers.size ());
 	}
 
 }
