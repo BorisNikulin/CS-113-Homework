289daf84bc50da3f9fecfc5770bf18727b80032f
Boris Nikulin <NikulinBE@gmail.com>
2016-11-14 10:14:42 -0800

    HW6 Project7 Change function complete

--- hw6-proj7/src/edu/miracosta/cs113/Change.java
+++ hw6-proj7/src/edu/miracosta/cs113/Change.java
@@ -4,12 +4,57 @@ package edu.miracosta.cs113;
 public class Change
 {
 	
+	public enum COIN
+	{
+		QUARTER(0.25), DIME(0.10), NICKEL(0.05), PENNY(0.01);
+		
+		private double worth;
+
+		COIN(double worth)
+		{
+			this.worth = worth;
+		}
+		
+		public double getWorth()
+		{
+			return worth;
+		}
+		
+		public int getCents()
+		{
+			return (int) (worth * 100);
+		}
+	}
+	
 	private Change () {}
 	
 	
 	public static void printChangeCombinations(double change)
 	{
-		
+		printChangeCombinationsRecursive(new int[COIN.values ().length], change);
+	}
+	
+	private static void printChangeCombinationsRecursive(int[] count, double change)
+	{
+		COIN[] coins = COIN.values ();
+		for(int i = 0; i < coins.length; ++i)
+		{
+			COIN coin = coins[i];
+			if((change - coin.getWorth ()) > 0)
+			{
+				count[i]++;
+				printChangeCombinationsRecursive(count, change);
+			}
+			else
+			{
+				printCoinCounts(count);
+			}
+		}
+	}
+	
+	private static void printCoinCounts(int[] count)
+	{
+		System.out.println (count);
 	}
 
 }
