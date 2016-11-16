0ef8aae99820c4c4bedb04989adb2270725755bd
Boris Nikulin <NikulinBE@gmail.com>
2016-11-15 17:32:49 -0800

    HW6 Project7 Change function works and provides no duplicates

--- hw6-proj7/src/edu/miracosta/cs113/Change.java
+++ hw6-proj7/src/edu/miracosta/cs113/Change.java
@@ -1,60 +1,70 @@
 
 package edu.miracosta.cs113;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Change
 {
-	
+
 	public enum COIN
 	{
-		QUARTER(0.25), DIME(0.10), NICKEL(0.05), PENNY(0.01);
 		
-		private double worth;
+		//least to greatest required
+//		QUARTER(25), DIME(10), NICKEL(5), PENNY(1);
+		PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
 
-		COIN(double worth)
+		private int centWorth;
+
+		COIN (int worth)
 		{
-			this.worth = worth;
+			this.centWorth = worth;
 		}
-		
-		public double getWorth()
+
+		public double getWorth ()
 		{
-			return worth;
+			return centWorth / 100.0;
 		}
-		
-		public int getCents()
+
+		public int getCents ()
 		{
-			return (int) (worth * 100);
+			return centWorth;
 		}
 	}
-	
-	private Change () {}
-	
-	
-	public static void printChangeCombinations(double change)
+
+	private Change ()
 	{
-		printChangeCombinationsRecursive(new int[COIN.values ().length], change);
 	}
-	
-	private static void printChangeCombinationsRecursive(int[] count, double change)
+
+	public static void printChangeCombinations (int change)
 	{
-		COIN[] coins = COIN.values ();
-		for(int i = 0; i < coins.length; ++i)
+		printChangeCombinationsRecursive (new int[COIN.values ().length], change, Arrays.asList (COIN.values ()));
+	}
+
+	private static void printChangeCombinationsRecursive (int[] count, int change, List<COIN> coins)
+	{
+		for (int i = 0; i < coins.size (); ++i)
 		{
-			COIN coin = coins[i];
-			if((change - coin.getWorth ()) > 0)
+			COIN coin = coins.get (i);
+			int reducedChange = change - coin.getCents ();
+			if (reducedChange > 0)
 			{
 				count[i]++;
-				printChangeCombinationsRecursive(count, change);
+				printChangeCombinationsRecursive (count, reducedChange, coins.subList (0, i + 1));
+				count[i]--;
 			}
-			else
+			else if (reducedChange == 0)
 			{
-				printCoinCounts(count);
+				count[i]++;
+				printCoinCounts (count);
+				count[i]--;
 			}
 		}
 	}
-	
-	private static void printCoinCounts(int[] count)
+
+	private static void printCoinCounts (int[] count)
 	{
-		System.out.println (count);
+		System.out.println (Arrays.toString (count));
 	}
 
 }
