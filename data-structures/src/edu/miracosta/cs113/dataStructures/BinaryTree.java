0183c2de3a37cc926c02b97897d85d9bd5114cff
Boris Nikulin <NikulinBE@gmail.com>
2016-11-22 22:36:53 -0800

    Data Structures BinaryTree documentation

--- data-structures/src/edu/miracosta/cs113/dataStructures/BinaryTree.java
+++ data-structures/src/edu/miracosta/cs113/dataStructures/BinaryTree.java
@@ -15,6 +15,9 @@ public class BinaryTree<E>
 
 	protected Node<? extends E> root;
 
+	/**
+	 * Creates a binary tree with a null root.
+	 */
 	public BinaryTree ()
 	{
 		root = null;
@@ -25,9 +28,20 @@ public class BinaryTree<E>
 		this.root = root;
 	}
 
+	/**
+	 * Creates a tree with root being data and having passed left and right
+	 * subtrees.
+	 * 
+	 * @param data
+	 *            - the root
+	 * @param left
+	 *            - left subtree
+	 * @param right
+	 *            - right subtree
+	 */
 	public BinaryTree (E data, BinaryTree<? extends E> left, BinaryTree<? extends E> right)
 	{
-		root = new Node<E> (
+		root = new Node<E>(
 				data,
 				left != null ? left.root : null,
 				right != null ? right.root : null);
@@ -35,12 +49,12 @@ public class BinaryTree<E>
 
 	public BinaryTree<E> getLeftSubTree ()
 	{
-		return root.left != null ? new BinaryTree<E> (root.left) : null;
+		return root.left != null ? new BinaryTree<E>(root.left) : null;
 	}
 
 	public BinaryTree<E> getRightSubTree ()
 	{
-		return root.right != null ? new BinaryTree<E> (root.right) : null;
+		return root.right != null ? new BinaryTree<E>(root.right) : null;
 	}
 
 	public E getData ()
@@ -53,51 +67,212 @@ public class BinaryTree<E>
 		return root.left == null && root.right == null;
 	}
 
+	/**
+	 * Performs the given action for each remaining element in pre-order until
+	 * all elements have been processed or the action throws an exception.
+	 * 
+	 * @param consumer
+	 *            - the action to be performed for each element
+	 * 
+	 * @see #forEach(Consumer, TRAVERSE)
+	 */
 	public void forEach (Consumer<? super E> consumer)
 	{
-		forEach (consumer, TRAVERSE.PRE_ORDER);
+		forEach(consumer, TRAVERSE.PRE_ORDER);
 	}
 
+	/**
+	 * Performs the given action for each remaining element and depth of the
+	 * element in pre-order until all elements have been processed or the action
+	 * throws an exception.
+	 * 
+	 * @param consumer
+	 *            - the action to be performed for each element where the second
+	 *            argument is the depth of the element
+	 */
 	public void forEachWithDepth (ObjIntConsumer<? super E> consumer)
 	{
-		forEachWithDepth (consumer, TRAVERSE.PRE_ORDER);
+		forEachWithDepth(consumer, TRAVERSE.PRE_ORDER);
 	}
 
+	/**
+	 * 
+	 * Performs the given action for each remaining element in the given order
+	 * until all elements have been processed or the action throws an exception.
+	 * 
+	 * @param consumer
+	 *            - the action to be performed for each element
+	 * @param traverseOrder
+	 *            - the order to traverse in
+	 * 
+	 * @see #forEach(Consumer)
+	 */
 	public void forEach (Consumer<? super E> consumer, TRAVERSE traverseOrder)
 	{
-		forEachWithDepth ( (element, depth) -> consumer.accept (element), traverseOrder);
+		forEachWithDepth( (element, depth) -> consumer.accept(element), traverseOrder);
 	}
 
+	/**
+	 * Performs the given action for each remaining element and depth of the
+	 * element in given order until all elements have been processed or the
+	 * action throws an exception.
+	 * 
+	 * @param consumer
+	 *            - the action to be performed for each element where the second
+	 *            argument is the depth of the element
+	 * @param traverseOrder
+	 *            - the order to traverse in
+	 * 
+	 * @see #forEachWithDepth(ObjIntConsumer)
+	 */
 	public void forEachWithDepth (ObjIntConsumer<? super E> consumer, TRAVERSE traverseOrder)
 	{
 		switch (traverseOrder)
 		{
 		case PRE_ORDER:
-			traversePreOrder (consumer, root, 0);
+			traversePreOrder(consumer, root, 0);
 			break;
 		case IN_ORDER:
-			traverseInOrder (consumer, root, 0);
+			traverseInOrder(consumer, root, 0);
 			break;
 		case POST_ORDER:
-			traversePostOrder (consumer, root, 0);
+			traversePostOrder(consumer, root, 0);
 			break;
 		default:
-			throw new UnsupportedOperationException (traverseOrder.toString () + " is not supported yet");
+			throw new UnsupportedOperationException(traverseOrder.toString() + " is not supported yet");
 			// although if one added an enum then he/she would of added the
 			// other necessary additions...
 		}
 	}
 
+	/**
+	 * Performs a mutable reduction operation on the elements of this tree using
+	 * a Collector in pre-order.
+	 * 
+	 * @param <A>
+	 *            - the intermediate accumulation type of the Collector
+	 * @param <R>
+	 *            - the type of the result
+	 * 
+	 * @param collector
+	 *            - the Collector describing the reduction
+	 * 
+	 * @return the result of the reduction
+	 * 
+	 * @see Collector
+	 * @see #collect(Collector, TRAVERSE)
+	 */
+	public <A, R> R collect (Collector<E, A, R> collector)
+	{
+		return collect(collector, TRAVERSE.PRE_ORDER);
+	}
+
+	/**
+	 * Performs a mutable reduction operation on {@link ElementDepthPair}, which
+	 * are composed of the element and the depth of that element, of this tree
+	 * using a Collector in pre-order.
+	 * 
+	 * @param <A>
+	 *            - the intermediate accumulation type of the Collector
+	 * @param <R>
+	 *            - the type of the result
+	 * 
+	 * @param collector
+	 *            - the Collector describing the reduction
+	 * 
+	 * @return the result of the reduction
+	 * 
+	 * @see Collector
+	 * @see #collectWithDepth(Collector, TRAVERSE)
+	 */
+	public <A, R> R collectWithDepth (Collector<ElementDepthPair<E>, A, R> collector)
+	{
+		return collectWithDepth(collector, TRAVERSE.PRE_ORDER);
+	}
+
+	/**
+	 * Performs a mutable reduction operation on the elements of this tree using
+	 * a Collector in post-order.
+	 * 
+	 * @param <A>
+	 *            - the intermediate accumulation type of the Collector
+	 * @param <R>
+	 *            - the type of the result
+	 * 
+	 * @param collector
+	 *            - the Collector describing the reduction
+	 * @param traverseOrder
+	 *            - the order to traverse in
+	 * 
+	 * @return the result of the reduction
+	 * 
+	 * @see Collector
+	 * @see #collect(Collector)
+	 */
+	public <A, R> R collect (Collector<E, A, R> collector, TRAVERSE traverseOrder)
+	{
+		Collector<ElementDepthPair<E>, A, R> modifiedCollector = Collector.of(
+				collector.supplier(),
+				(container, elementDepthTuple) -> collector.accumulator()
+						.accept(container, elementDepthTuple.getElement()),
+				collector.combiner(),
+				collector.finisher(),
+				// may type erasure genetics feel the pain i've felt for these
+				// couple hours
+				collector.characteristics().toArray(
+						new Collector.Characteristics[collector.characteristics().size()]));
+
+		return collectWithDepth(modifiedCollector, traverseOrder);
+	}
+
+	/**
+	 * 
+	 * Performs a mutable reduction operation on {@link ElementDepthPair}, which
+	 * are composed of the element and the depth of that element, of this tree
+	 * using a Collector in the given order.
+	 * 
+	 * @param <A>
+	 *            - the intermediate accumulation type of the Collector
+	 * @param <R>
+	 *            - the type of the result
+	 * 
+	 * @param collector
+	 *            - the Collector describing the reduction
+	 * @param traverseOrder
+	 * 
+	 * @return the result of the reduction
+	 * 
+	 * @see Collector
+	 * @see #collectWithDepth(Collector)
+	 */
+	@SuppressWarnings("unchecked")
+	public <A, R> R collectWithDepth (Collector<ElementDepthPair<E>, A, R> collector, TRAVERSE traverseOrder)
+	{
+		A container = collector.supplier().get();
+
+		forEachWithDepth( (element, depth) -> collector.accumulator()
+				.accept(container, new ElementDepthPair<E>(element, depth)));
+
+		if (collector.characteristics().contains(Collector.Characteristics.IDENTITY_FINISH))
+		{
+			return (R) container;
+		}
+		else
+		{
+			return collector.finisher().apply(container);
+		}
+	}
+
 	private void traversePreOrder (ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
 	{
-		consumer.accept (root.data, depth);
+		consumer.accept(root.data, depth);
 		if (root.left != null)
 		{
-			traversePreOrder (consumer, root.right, depth + 1);
+			traversePreOrder(consumer, root.right, depth + 1);
 		}
 		if (root.right != null)
 		{
-			traversePreOrder (consumer, root.right, depth + 1);
+			traversePreOrder(consumer, root.right, depth + 1);
 		}
 	}
 
@@ -105,12 +280,12 @@ public class BinaryTree<E>
 	{
 		if (root.left != null)
 		{
-			traversePreOrder (consumer, root.right, depth + 1);
+			traversePreOrder(consumer, root.right, depth + 1);
 		}
-		consumer.accept (root.data, depth);
+		consumer.accept(root.data, depth);
 		if (root.right != null)
 		{
-			traversePreOrder (consumer, root.right, depth + 1);
+			traversePreOrder(consumer, root.right, depth + 1);
 		}
 	}
 
@@ -118,57 +293,13 @@ public class BinaryTree<E>
 	{
 		if (root.left != null)
 		{
-			traversePreOrder (consumer, root.right, depth + 1);
+			traversePreOrder(consumer, root.right, depth + 1);
 		}
 		if (root.right != null)
 		{
-			traversePreOrder (consumer, root.right, depth + 1);
+			traversePreOrder(consumer, root.right, depth + 1);
 		}
-		consumer.accept (root.data, depth);
-	}
-
-	public <A, R> R collect (Collector<E, A, R> collector)
-	{
-		return collect (collector, TRAVERSE.PRE_ORDER);
-	}
-
-	public <A, R> R collectWithDepth (Collector<ElementDepthPair<E>, A, R> collector)
-	{
-		return collectWithDepth (collector, TRAVERSE.PRE_ORDER);
-	}
-
-	public <A, R> R collect (Collector<E, A, R> collector, TRAVERSE traverseOrder)
-	{
-		Collector<ElementDepthPair<E>, A, R> modifiedCollector = Collector.of (
-				collector.supplier (),
-				(container, elementDepthTuple) -> collector.accumulator ()
-						.accept (container, elementDepthTuple.getElement ()),
-				collector.combiner (),
-				collector.finisher (),
-				// may type erasure genetics feel the pain i've felt for these
-				// couple hours
-				collector.characteristics ().toArray (
-						new Collector.Characteristics[collector.characteristics ().size ()]));
-
-		return collectWithDepth (modifiedCollector, traverseOrder);
-	}
-
-	@SuppressWarnings("unchecked")
-	public <A, R> R collectWithDepth (Collector<ElementDepthPair<E>, A, R> collector, TRAVERSE traverseOrder)
-	{
-		A container = collector.supplier ().get ();
-
-		forEachWithDepth ( (element, depth) -> collector.accumulator ()
-				.accept (container, new ElementDepthPair<E> (element, depth)));
-
-		if (collector.characteristics ().contains (Collector.Characteristics.IDENTITY_FINISH))
-		{
-			return (R) container;
-		}
-		else
-		{
-			return collector.finisher ().apply (container);
-		}
+		consumer.accept(root.data, depth);
 	}
 
 	public enum TRAVERSE
@@ -197,6 +328,14 @@ public class BinaryTree<E>
 		}
 	}
 
+	/**
+	 * Tuple for an element and depth pair. Contains accessors for both.
+	 * 
+	 * @see ElementDepthPair#getElement()
+	 * @see ElementDepthPair#getDepth()
+	 * 
+	 * @author Boris
+	 */
 	private static class ElementDepthPair<E>
 	{
 		private E	element;
