03197c0b1a5c3259311239c7e5a10d75438187a8
Boris Nikulin <NikulinBE@gmail.com>
2016-11-23 04:25:17 -0800

    Data Structures BinaryTree debugging and a map method
    map method is useful in conjunction with read where the string may be then mapped to whatever suitable type is needed.

--- data-structures/src/edu/miracosta/cs113/dataStructures/BinaryTree.java
+++ data-structures/src/edu/miracosta/cs113/dataStructures/BinaryTree.java
@@ -4,8 +4,10 @@ package edu.miracosta.cs113.dataStructures;
 import java.util.Scanner;
 import java.util.StringJoiner;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.ObjIntConsumer;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * @author Boris
@@ -48,28 +50,28 @@ public class BinaryTree<E>
 				right != null ? right.root : null);
 	}
 
-	public BinaryTree<E> getLeftSubTree ()
+	public BinaryTree<E> getLeftSubTree()
 	{
 		return root.left != null ? new BinaryTree<E>(root.left) : null;
 	}
 
-	public BinaryTree<E> getRightSubTree ()
+	public BinaryTree<E> getRightSubTree()
 	{
 		return root.right != null ? new BinaryTree<E>(root.right) : null;
 	}
 
-	public E getData ()
+	public E getData()
 	{
 		return root != null ? root.data : null;
 	}
 
 	/**
-	 * Returns whether the local root of this tree is a leaf.
-	 * If root is null then a null pointer exception is thrown.
+	 * Returns whether the local root of this tree is a leaf. If root is null
+	 * then a null pointer exception is thrown.
 	 * 
 	 * @return true if and only if left and right subtrees are null
 	 */
-	public boolean isLeaf ()
+	public boolean isLeaf()
 	{
 		return root.left == null && root.right == null;
 	}
@@ -83,7 +85,7 @@ public class BinaryTree<E>
 	 * 
 	 * @see #forEach(Consumer, TRAVERSE)
 	 */
-	public void forEach (Consumer<? super E> consumer)
+	public void forEach(Consumer<? super E> consumer)
 	{
 		forEach(consumer, TRAVERSE.PRE_ORDER);
 	}
@@ -97,7 +99,7 @@ public class BinaryTree<E>
 	 *            - the action to be performed for each element where the second
 	 *            argument is the depth of the element
 	 */
-	public void forEachWithDepth (ObjIntConsumer<? super E> consumer)
+	public void forEachWithDepth(ObjIntConsumer<? super E> consumer)
 	{
 		forEachWithDepth(consumer, TRAVERSE.PRE_ORDER);
 	}
@@ -114,9 +116,9 @@ public class BinaryTree<E>
 	 * 
 	 * @see #forEach(Consumer)
 	 */
-	public void forEach (Consumer<? super E> consumer, TRAVERSE traverseOrder)
+	public void forEach(Consumer<? super E> consumer, TRAVERSE traverseOrder)
 	{
-		forEachWithDepth( (element, depth) -> consumer.accept(element), traverseOrder);
+		forEachWithDepth((element, depth) -> consumer.accept(element), traverseOrder);
 	}
 
 	/**
@@ -132,23 +134,26 @@ public class BinaryTree<E>
 	 * 
 	 * @see #forEachWithDepth(ObjIntConsumer)
 	 */
-	public void forEachWithDepth (ObjIntConsumer<? super E> consumer, TRAVERSE traverseOrder)
+	public void forEachWithDepth(ObjIntConsumer<? super E> consumer, TRAVERSE traverseOrder)
 	{
-		switch (traverseOrder)
+		if (root != null)
 		{
-		case PRE_ORDER:
-			traversePreOrder(consumer, root, 0);
-			break;
-		case IN_ORDER:
-			traverseInOrder(consumer, root, 0);
-			break;
-		case POST_ORDER:
-			traversePostOrder(consumer, root, 0);
-			break;
-		default:
-			throw new UnsupportedOperationException(traverseOrder.toString() + " is not supported yet");
-			// although if one added an enum then he/she would of added the
-			// other necessary additions...
+			switch (traverseOrder)
+			{
+			case PRE_ORDER:
+				traversePreOrder(consumer, root, 0);
+				break;
+			case IN_ORDER:
+				traverseInOrder(consumer, root, 0);
+				break;
+			case POST_ORDER:
+				traversePostOrder(consumer, root, 0);
+				break;
+			default:
+				throw new UnsupportedOperationException(traverseOrder.toString() + " is not supported yet");
+				// although if one added an enum then he/she would of added the
+				// other necessary additions...
+			}
 		}
 	}
 
@@ -169,7 +174,7 @@ public class BinaryTree<E>
 	 * @see Collector
 	 * @see #collect(Collector, TRAVERSE)
 	 */
-	public <A, R> R collect (Collector<E, A, R> collector)
+	public <A, R> R collect(Collector<E, A, R> collector)
 	{
 		return collect(collector, TRAVERSE.PRE_ORDER);
 	}
@@ -192,7 +197,7 @@ public class BinaryTree<E>
 	 * @see Collector
 	 * @see #collectWithDepth(Collector, TRAVERSE)
 	 */
-	public <A, R> R collectWithDepth (Collector<ElementDepthPair<E>, A, R> collector)
+	public <A, R> R collectWithDepth(Collector<ElementDepthPair<E>, A, R> collector)
 	{
 		return collectWithDepth(collector, TRAVERSE.PRE_ORDER);
 	}
@@ -216,7 +221,7 @@ public class BinaryTree<E>
 	 * @see Collector
 	 * @see #collect(Collector)
 	 */
-	public <A, R> R collect (Collector<E, A, R> collector, TRAVERSE traverseOrder)
+	public <A, R> R collect(Collector<E, A, R> collector, TRAVERSE traverseOrder)
 	{
 		Collector<ElementDepthPair<E>, A, R> modifiedCollector = Collector.of(
 				collector.supplier(),
@@ -253,11 +258,11 @@ public class BinaryTree<E>
 	 * @see #collectWithDepth(Collector)
 	 */
 	@SuppressWarnings("unchecked")
-	public <A, R> R collectWithDepth (Collector<ElementDepthPair<E>, A, R> collector, TRAVERSE traverseOrder)
+	public <A, R> R collectWithDepth(Collector<ElementDepthPair<E>, A, R> collector, TRAVERSE traverseOrder)
 	{
 		A container = collector.supplier().get();
 
-		forEachWithDepth( (element, depth) -> collector.accumulator()
+		forEachWithDepth((element, depth) -> collector.accumulator()
 				.accept(container, new ElementDepthPair<E>(element, depth)));
 
 		if (collector.characteristics().contains(Collector.Characteristics.IDENTITY_FINISH))
@@ -270,7 +275,7 @@ public class BinaryTree<E>
 		}
 	}
 
-	private void traversePreOrder (ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
+	private void traversePreOrder(ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
 	{
 		consumer.accept(root.data, depth);
 		if (root.left != null)
@@ -283,35 +288,91 @@ public class BinaryTree<E>
 		}
 	}
 
-	private void traverseInOrder (ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
+	private void traverseInOrder(ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
 	{
 		if (root.left != null)
 		{
-			traversePreOrder(consumer, root.left, depth + 1);
+			traverseInOrder(consumer, root.left, depth + 1);
 		}
 		consumer.accept(root.data, depth);
 		if (root.right != null)
 		{
-			traversePreOrder(consumer, root.right, depth + 1);
+			traverseInOrder(consumer, root.right, depth + 1);
 		}
 	}
 
-	private void traversePostOrder (ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
+	private void traversePostOrder(ObjIntConsumer<? super E> consumer, Node<? extends E> root, int depth)
 	{
 		if (root.left != null)
 		{
-			traversePreOrder(consumer, root.left, depth + 1);
+			traversePostOrder(consumer, root.left, depth + 1);
 		}
 		if (root.right != null)
 		{
-			traversePreOrder(consumer, root.right, depth + 1);
+			traversePostOrder(consumer, root.right, depth + 1);
 		}
 		consumer.accept(root.data, depth);
 	}
 
-	public String toString ()
+	@Override
+	public int hashCode()
 	{
-		return collect(Collector.of( () -> new StringJoiner(", ", "[", "]"),
+		final int prime = 31;
+		return 31 + collect(Collectors.summingInt(element -> prime * element.hashCode()));
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (!(obj instanceof BinaryTree)) return false;
+		@SuppressWarnings("unchecked") BinaryTree<E> other = (BinaryTree<E>) obj;
+		if (root == null && other.root != null) return false;
+		if (!recursiveEquals(root, other.root)) return false;
+		return true;
+	}
+
+	/**
+	 * Recursively compare both trees
+	 * 
+	 * @param root
+	 *            - not null
+	 * @param otherRoot
+	 * @return
+	 */
+	private boolean recursiveEquals(Node<? extends E> root, Node<? extends E> otherRoot)
+	{
+		// TODO some early exit that stops the other branch
+		// exceptions? or a global stop var like with a private interal class
+		boolean isEqual = root.equals(otherRoot);
+		if (isEqual)
+		{
+			return false;
+		}
+
+		if ((root.left != null ^ otherRoot.left != null) || (root.right != null ^ otherRoot.right != null))
+		{
+			return false;
+		}
+
+		if (root.left != null && otherRoot.left != null)
+		{
+			return recursiveEquals(root.left, otherRoot.left);
+		}
+
+		if (root.right != null && otherRoot.right != null)
+		{
+			return recursiveEquals(root.right, otherRoot.right);
+		}
+
+		// both roots are null
+		return true;
+	}
+
+	public String toString()
+	{
+		return collect(Collector.of(() -> new StringJoiner(", ", "[", "]"),
 				(strJnr, element) -> strJnr.add(element.toString()),
 				StringJoiner::merge,
 				Collector.Characteristics.IDENTITY_FINISH)).toString();
@@ -320,13 +381,14 @@ public class BinaryTree<E>
 	/**
 	 * Reads a scanner, next by next, and constructs a tree in pre-order
 	 * 
-	 * @param scn - the scanner to read from
+	 * @param scn
+	 *            - the scanner to read from
 	 * @return a {@link BinaryTree} in pre-order
 	 */
 	public static BinaryTree<String> readBinaryTree(Scanner scn)
 	{
 		String data = scn.next();
-		if(data.equals("null"))
+		if (data.equals("null"))
 		{
 			return null;
 		}
@@ -334,7 +396,32 @@ public class BinaryTree<E>
 		BinaryTree<String> right = readBinaryTree(scn);
 		return new BinaryTree<String>(data, left, right);
 	}
-	
+
+	public <R> BinaryTree<R> map(Function<E, R> mapper)
+	{
+		if (root != null)
+		{
+			return map(mapper, root);
+		}
+		return new BinaryTree<R>();
+	}
+
+	private <R> BinaryTree<R> map(Function<E, R> mapper, Node<? extends E> root)
+	{
+		R mappedData = mapper.apply(root.data);
+		BinaryTree<R> left = null;
+		BinaryTree<R> right = null;
+		if (root.left != null)
+		{
+			left = map(mapper, root.left);
+		}
+		if (root.right != null)
+		{
+			right = map(mapper, root.right);
+		}
+		return new BinaryTree<R>(mappedData, left, right);
+	}
+
 	public enum TRAVERSE
 	{
 		PRE_ORDER, IN_ORDER, POST_ORDER, // TODO depth and breadth first;
@@ -359,7 +446,7 @@ public class BinaryTree<E>
 			this.left = left;
 			this.right = right;
 		}
-		
+
 		public String toString()
 		{
 			return data != null ? data.toString() : "null";
@@ -367,14 +454,15 @@ public class BinaryTree<E>
 	}
 
 	/**
-	 * Tuple for an element and depth pair. Contains accessors for both.
+	 * Tuple for an element and depth pair. Contains accessors for both. All
+	 * fields are non null.
 	 * 
 	 * @see ElementDepthPair#getElement()
 	 * @see ElementDepthPair#getDepth()
 	 * 
 	 * @author Boris
 	 */
-	private static class ElementDepthPair<E>
+	public static class ElementDepthPair<E>
 	{
 		private E	element;
 		private int	depth;
@@ -385,12 +473,12 @@ public class BinaryTree<E>
 			this.depth = depth;
 		}
 
-		public E getElement ()
+		public E getElement()
 		{
 			return element;
 		}
 
-		public int getDepth ()
+		public int getDepth()
 		{
 			return depth;
 		}
